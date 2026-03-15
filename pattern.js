cc0289c7f28a6d7010c7658dcf7d5ecc96ce3ad9
BPatil2727 <152608269+BPatil2727@users.noreply.github.com>
2026-03-15 10:31:38 +0530

    Implement number pattern printing in pattern.js
    Add a pattern printing script that displays numbers in rows.

--- pattern.js
+++ pattern.js
@@ -1,1 +1,13 @@
+let num = 1;
+let rows = 5;
 
+for (let i = 1; i <= rows; i++) {
+    let line = "";
+
+    for (let j = 1; j <= i; j++) {
+        line += num + " ";
+        num++;
+    }
+
+    console.log(line.trim());
+}
